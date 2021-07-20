1ddb28257a6347ebe21c45604e93a52093c3ce84
Ayush Yadav <ayushiit2003@gmail.com>
2021-07-21 02:47:37 +0530

    Added animated button

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,7 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 
 //class is a blueprint and copy of blueprint is object
-class LoginPage extends StatelessWidget {
+//We have made this class statefull as we are implemeting welcome+what user type in username
+//This will change the state of the widget and required to be load again and again when user inputs
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+//Underscore berfore LoginPageState means making it private
+//Means this login class cannot be used by anything outsite of this file
+class _LoginPageState extends State<
+    LoginPage> //This state class enable us to refresh this widget/screen
+{
+  String name = "";
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -22,7 +35,7 @@ class LoginPage extends StatelessWidget {
               height: 20.0,
             ), //gives particular type of box which is blank
             Text(
-              "Welcome",
+              "Welcome $name",
               style: TextStyle(
                 fontSize: 28,
                 fontWeight: FontWeight.bold,
@@ -36,10 +49,20 @@ class LoginPage extends StatelessWidget {
                   const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
               child: Column(
                 children: [
+                  //Here textformfield is also a statefull widget as it is extend to stateful widget(ctrl+click on it to see)
+                  //This means every widget can maintain its own state (statefull/stateless)
+                  //This result in overall speeed improvement in app as if we make full app statefull then every thing will be created again and agian on that few change which are not even nessary
                   TextFormField(
-                    decoration: InputDecoration(
-                        hintText: "Enter username", labelText: "Username"),
-                  ),
+                      decoration: InputDecoration(
+                          hintText: "Enter username", labelText: "Username"),
+                      onChanged: (value) {
+                        name = value;
+                        setState(
+                            () {}); //This setstate call this build function again when user input/on change and redraws the screen
+                      } //This onchaged will give a callback of type string whenever there is a change/input from user
+                      //value is the argument which is the input in text form field and this variable name can be anyhing
+                      //this value is then stored in name
+                      ),
                   TextFormField(
                     decoration: InputDecoration(
                         hintText: "Enter password", labelText: "Password"),
@@ -48,13 +71,50 @@ class LoginPage extends StatelessWidget {
                   SizedBox(
                     height: 40.0,
                   ),
-                  ElevatedButton(
-                    child: Text("Login"),
-                    style: TextButton.styleFrom(minimumSize: Size(150, 40)),
-                    onPressed: () {
-                      //Naviagtor is used to link the routes and navigate it from one page to another
+                  // ElevatedButton(
+                  //   child: Text("Login"),
+                  //   style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                  //   onPressed: () {
+                  //     //Naviagtor is used to link the routes and navigate it from one page to another
+                  //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //   },
+                  // )
+                  //Now We will see animated button
+                  //two make a any widget clickable there are two methods:
+                  //1.wrap it with justerdetector (no animation)
+                  //2.wrap it wiht inkwell (animated)
+                  InkWell(
+                    onTap: () async {
+                      setState(() {
+                        changeButton = true;
+                      });
+
+                      //await will make it wait for that durataion and also to make await work the function should be async in which it is used
+                      await Future.delayed(Duration(seconds: 1));
                       Navigator.pushNamed(context, MyRoutes.homeRoute);
                     },
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      width: changeButton ? 50 : 150,
+                      height: 50,
+                      // color: Colors.deepPurple,
+                      // We cannot use color at two place i.e here and in decoration
+                      child: changeButton
+                          ? Icon(Icons.done)
+                          : Text(
+                              "Login",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18),
+                            ),
+                      decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          // shape: changeButton ? BoxShape.circle : BoxShape.rectangle,
+                          //Box shape and border radius cannot be used together
+                          borderRadius:
+                              BorderRadius.circular(changeButton ? 50 : 8)),
+                    ),
                   )
                 ],
               ),
