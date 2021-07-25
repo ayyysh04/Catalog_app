b9d3630fd86035c93e128f8b5560f93307a9b8e3
Ayush Yadav <ayushiit2003@gmail.com>
2021-07-26 02:45:57 +0530

    Velociyx - added new UI to Home_page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,8 @@ import 'package:flutter/services.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 import 'package:flutter_catalog/widgets/item_widget.dart';
+import 'package:flutter_catalog/widgets/themes.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -60,96 +62,225 @@ class _HomePageState extends State<HomePage> {
     // final dummylist = List.generate(25,(index) => CatalogModel.items[0]); //This will generate adummylist of type item[0]
     //Used to see how if there were many items will see
     return Scaffold(
-      appBar: AppBar(
-        // backgroundColor: Colors.white,
-        // elevation: 0.0, //Removes the shadow below the appbar
-        // iconTheme: IconThemeData(color: Colors.black), //This makes all color ,styling,etc related to icons in appbar eg:drawer icon
-        ///We have made an seprate theme data in main.dart so that it can used everywhere in an easier manner
-        title: Text(
-          "Catalog App",
-          // style: TextStyle(color: Colors.black),
-        ),
-      ),
-      body:
-          // Center(
-          // // single widget inside wiget : child
-          ////multiple widget inside wiget : children ,eg:row,coloumn,listview
-          // child: Container(
-          //     child: Text("Welcome"),
-          //   ),
-          // ),
-          Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
-            ?
-            //Using list view
-            //  ListView
-            //     .builder //create a list and only render the list which are on screen when u scroll down the rendering of rest items starts
-            //     (
-            //     itemCount: CatalogModel.items!.length,
-            //     itemBuilder: (context, index) => ItemWidget
-            //         //used short hand => instead of return
-            //         (
-            //       item: CatalogModel.items![index],
-            //     ),
-            //   )
-            //using grid view
-            GridView.builder(
-                //grid delegrate tels to builder that how many items should be there in row or coloumn
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  //crossaxix: horizontal axis
-                  //mainaxis : Vertical axis
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 16,
-                  crossAxisSpacing: 16,
-                ),
+        backgroundColor: MyTheme.creamColor,
+        // appBar: AppBar(
+        //   // backgroundColor: Colors.white,
+        //   // elevation: 0.0, //Removes the shadow below the appbar
+        //   // iconTheme: IconThemeData(color: Colors.black), //This makes all color ,styling,etc related to icons in appbar eg:drawer icon
+        //   ///We have made an seprate theme data in main.dart so that it can used everywhere in an easier manner
+        //   title: Text(
+        //     "Catalog App",
+        //     // style: TextStyle(color: Colors.black),
+        //   ),
+        // ),
+        body:
+            //     // Center(
+            //     // // single widget inside wiget : child
+            //     ////multiple widget inside wiget : children ,eg:row,coloumn,listview
+            //     // child: Container(
+            //     //     child: Text("Welcome"),
+            //     //   ),
+            //     // ),
+            //     Padding(
+            //   padding: const EdgeInsets.all(16.0),
+            //   child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
+            // ?
+            //       //Using list view
+            //       //  ListView
+            //       //     .builder //create a list and only render the list which are on screen when u scroll down the rendering of rest items starts
+            //       //     (
+            //       //     itemCount: CatalogModel.items!.length,
+            //       //     itemBuilder: (context, index) => ItemWidget
+            //       //         //used short hand => instead of return
+            //       //         (
+            //       //       item: CatalogModel.items![index],
+            //       //     ),
+            //       //   )
+            //       //using grid view
+            //       GridView.builder(
+            //           //grid delegrate tels to builder that how many items should be there in row or coloumn
+            //           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            //             //crossaxix: horizontal axis
+            //             //mainaxis : Vertical axis
+            //             crossAxisCount: 2,
+            //             mainAxisSpacing: 16,
+            //             crossAxisSpacing: 16,
+            //           ),
 
-                itemBuilder: (context, index) {
-                  final item = CatalogModel.items![index];
-                  return Card(
-                    clipBehavior: Clip.antiAlias,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    child: GridTile(
-                        header: Container(
-                          child: Text(
-                            item.name,
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          decoration: BoxDecoration(
-                            color: Colors.deepPurple,
-                          ),
-                          padding: const EdgeInsets.all(
-                              12), //constant :make easier for compiler as it doesnt make the padding set agian again
-                        ),
-                        child: Container(
-                          child: Image.network(
-                            item.image,
-                          ),
-                          // margin: EdgeInsets.all(16.0),
-                          padding: EdgeInsets.symmetric(
-                            vertical: 50,
-                          ),
-                        ),
-                        footer: Container(
-                          child: Text(
-                            "\$${item.price.toString()}",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          decoration: BoxDecoration(
-                            color: Colors.black,
-                          ),
-                          padding: const EdgeInsets.all(
-                              12), //constant :make easier for compiler as it doesnt make the padding set agian again
-                        )),
-                  );
-                },
-                itemCount: CatalogModel.items!.length,
-              )
-            : Center(child: CircularProgressIndicator()),
-      ),
-      drawer: MyDrawer(),
+            //           itemBuilder: (context, index) {
+            //             final item = CatalogModel.items![index];
+            //             return Card(
+            //               clipBehavior: Clip.antiAlias,
+            //               shape: RoundedRectangleBorder(
+            //                   borderRadius: BorderRadius.circular(10)),
+            //               child: GridTile(
+            //                   header: Container(
+            //                     child: Text(
+            //                       item.name,
+            //                       style: TextStyle(color: Colors.white),
+            //                     ),
+            //                     decoration: BoxDecoration(
+            //                       color: Colors.deepPurple,
+            //                     ),
+            //                     padding: const EdgeInsets.all(
+            //                         12), //constant :make easier for compiler as it doesnt make the padding set agian again
+            //                   ),
+            //                   child: Container(
+            //                     child: Image.network(
+            //                       item.image,
+            //                     ),
+            //                     // margin: EdgeInsets.all(16.0),
+            //                     padding: EdgeInsets.symmetric(
+            //                       vertical: 50,
+            //                     ),
+            //                   ),
+            //                   footer: Container(
+            //                     child: Text(
+            //                       "\$${item.price.toString()}",
+            //                       textAlign: TextAlign.center,
+            //                       style: TextStyle(color: Colors.white),
+            //                     ),
+            //                     decoration: BoxDecoration(
+            //                       color: Colors.black,
+            //                     ),
+            //                     padding: const EdgeInsets.all(
+            //                         12), //constant :make easier for compiler as it doesnt make the padding set agian again
+            //                   )),
+            //             );
+            //           },
+            //           itemCount: CatalogModel.items!.length,
+            //         )
+            //       : Center(child: CircularProgressIndicator()),
+            // ),
+            // drawer: MyDrawer(),
+
+            //Using velocityx
+            SafeArea //Safearea keeps everything inside safe area i.e away from system ui (notification bar wher time,battery other thongs are there & navigation buttons in vase of on display ones)
+                (
+          child: Container(
+            padding: Vx.m32, //same as EdgeInsets.all(32),
+
+            child: Column(children: [
+              CatalogHeader(),
+              //In children and many others we can use if else directly
+              if (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
+                Cataloglist()
+                    .expand() //we are using velocityx syntax ,Ps.We can wrap this with expanded() widget
+              //As we areusing list in coloumn so Using an Expanded widget makes a child of a Row, Column, or Flex expand to fill the available space along the main axis
+              else
+                Center(
+                  child: CircularProgressIndicator(),
+                )
+            ]),
+          ),
+        ));
+  }
+}
+
+//Catalog Header
+class CatalogHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // "Catalog App".text.make(),//velocityx syntax
+        // Text("Catalog App"),same as above
+        "Catalog App".text.xl5.bold.color(MyTheme.darkBluishColor).make(),
+        "Trending products".text.xl2.make(),
+      ],
     );
+  }
+}
+
+//Catalog list
+class Cataloglist extends StatelessWidget {
+  const Cataloglist({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap:
+          true, // With shrinkWrap: true, you can change this behavior so that the ListView only occupies the space it needs (it will still scroll when there more items).
+      itemCount: CatalogModel.items!.length,
+      itemBuilder: (context, index) {
+        final catalog = CatalogModel.items![index];
+        return CatalogItem(
+            catalog:
+                catalog); //this is how named argumet.optional argument is passed
+      },
+    );
+  }
+}
+
+class CatalogItem extends StatelessWidget {
+  final Item? catalog;
+
+  const CatalogItem({Key? key, this.catalog}) //{} make the argument optional
+      : super(
+            key:
+                key); //we r not using assert as null saftey is there and required should be used insteadof @required
+  @override
+  Widget build(BuildContext context) {
+    return VxBox(
+      child: Row(
+        children: [
+          CatalogImage(image: catalog!.image),
+          Expanded(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              catalog!.name.text.lg.color(MyTheme.darkBluishColor).bold.make(),
+              catalog!.desc.text
+                  .textStyle(context.captionStyle!)
+                  .make(), //captionstyle makes the text loke like a caption text
+              10.heightBox //Same as sized box with height 10
+              ,
+              ButtonBar //Buttonbar - bar where we can add too many buttons
+                  (
+                alignment: MainAxisAlignment.spaceBetween,
+                buttonPadding: Vx.m0,
+                children: [
+                  "\$${catalog!.price}".text.bold.xl.make(),
+                  ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(MyTheme.darkBluishColor),
+                        shape: MaterialStateProperty.all(
+                          StadiumBorder(),
+                        ),
+                      ),
+                      child: "Buy".text.make())
+                ],
+              ).pOnly(right: 8.0)
+            ],
+          ))
+        ],
+      ),
+    ).white.rounded.square(150).make().py16(); //vx box is similar to container
+    //  .white.square(100).py16.make() //this will give padding inside the list
+  }
+}
+
+class CatalogImage extends StatelessWidget {
+  final String image;
+
+  const CatalogImage({Key? key, required this.image}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(image)
+            .box
+            .rounded
+            .p8
+            .color(MyTheme.creamColor)
+            .make()
+            .p16()
+            .w40(
+              context,
+            ) //whalf make the width equal to the half of the screen it refrence to context
+        ;
   }
 }
--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -5,7 +5,8 @@ import 'package:google_fonts/google_fonts.dart';
 class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
         primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
+        fontFamily: GoogleFonts.poppins().fontFamily,
+        // GoogleFonts.lato().fontFamily,
         appBarTheme: AppBarTheme(
           color: Colors.white,
           elevation: 0.0,
@@ -19,4 +20,7 @@ class MyTheme {
 
   static ThemeData darkTheme(BuildContext context) =>
       ThemeData(brightness: Brightness.dark);
+
+  static Color creamColor = Color(0xfff5f5f5);
+  static Color darkBluishColor = Color(0xff403b58);
 }
